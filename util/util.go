f532ddaf9d269ce571dd7cdd19badb06a49467f5
rhaidiz <rhaidiz@protonmail.com>
2019-07-27 09:50:52 +0200

    Implement request to string method

--- util/util.go
+++ util/util.go
@@ -7,7 +7,21 @@ import (
 )
 
 func RequestToString(r *http.Request) string {
-	//TODO: add me
+	if r == nil {
+		return ""
+	}
+	ret := fmt.Sprintf("%s %s %s\nHost: %s\n", r.Method, r.URL.Path, r.Proto, r.Host)
+	for k, v := range r.Header {
+		values := ""
+		for _, s := range v {
+			values = values + s
+		}
+		ret = ret + fmt.Sprintf("%s: %s\n", k, values)
+	}
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	if len(bodyBytes) > 0 {
+		ret = ret + fmt.Sprintf("\n%s", string(bodyBytes))
+	}
 	return ""
 }
 
